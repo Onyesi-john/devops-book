b6c97e31e329ed4d2dce9c56c202fa8dc1253900
Onyesi-john <72194579+Onyesi-john@users.noreply.github.com>
2025-01-24 08:41:17 +0100

    Update app.js

--- ch1/sample-app/app.js
+++ ch1/sample-app/app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');          
 });
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 3000; 
 server.listen(port,() => {
   console.log(`Listening on port ${port}`);
 });
